812f32b60a508d7ad03482502b2ce07d0751d6aa
ss35789 <ss35789@naver.com>
2022-10-31 18:50:02 +0900

    List에 들어갈 데이터 형태

--- app/src/main/java/com/kreimben/android_sns_app/ListLayout.kt.kt
+++ app/src/main/java/com/kreimben/android_sns_app/ListLayout.kt.kt
@@ -1,4 +1,10 @@
 package com.kreimben.android_sns_app
 
-class ListLayout (val content: String, val created_at: String, val image_url: String, val title:String, val user: String){
+import java.security.Timestamp
+
+class ListLayout (val content: String?,
+                  val created_at: com.google.firebase.Timestamp?,
+                  val image_url: String?,
+                  val title:String?,
+                  val user: String?){
 }
