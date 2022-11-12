2751dc1e80a24f60c8eafceb9a46b2c74979cf29
ss35789 <ss35789@naver.com>
2022-11-12 19:02:06 +0900

    uid로 불러오기

--- app/src/main/java/com/kreimben/android_sns_app/EditProfileActivity.kt
+++ app/src/main/java/com/kreimben/android_sns_app/EditProfileActivity.kt
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
@@ -21,7 +22,7 @@ class EditProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditProfileBinding
     private var imageURL: String? = null
 
-    //    private val db: FirebaseFirestore = Firebase.firestore
+    //private val db: FirebaseFirestore = Firebase.firestore
     private val storage: FirebaseStorage = FirebaseStorage.getInstance()
     private val imageResult = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
@@ -118,9 +119,9 @@ class EditProfileActivity : AppCompatActivity() {
         val currentUser = FirebaseAuth.getInstance().currentUser
 
         if (currentUser != null) {
-            val email = currentUser.email
-            if (email != null) {
-                Firebase.firestore.collection("user").document(email).get()
+            val uid = currentUser.uid
+            if (uid != null) {
+                Firebase.firestore.collection("user").document(uid).get()
                     .addOnSuccessListener {
                         if (it != null) {
                             val nickname = it.data!!.get("displayname").toString()
--- app/src/main/java/com/kreimben/android_sns_app/FirestoreHelper.kt
+++ app/src/main/java/com/kreimben/android_sns_app/FirestoreHelper.kt
@@ -117,5 +117,7 @@ class FirestoreHelper {
         }
     }
 
+
+
 }
 
