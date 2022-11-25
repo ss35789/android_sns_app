e0072445f265bb0b8ca02fd610764821ae7b5633
ss35789 <ss35789@naver.com>
2022-11-25 17:57:31 +0900

    Update MainActivity.kt

--- app/src/main/java/com/kreimben/android_sns_app/MainActivity.kt
+++ app/src/main/java/com/kreimben/android_sns_app/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout.OnRefreshListener
